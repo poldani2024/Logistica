757a3723793a7b4a1329d78ebd3bff63d67ecbc0
poldani2024 <pedro.l.oldani@gmail.com>
2026-01-15 12:31:28 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,10 +1,7 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+ 
   const firebaseConfig = {
     apiKey: "AIzaSyATJicgCiFF3d_EUUw5GQbzPox5M2uSkcw",
     authDomain: "logistica-d964e.firebaseapp.com",
@@ -14,9 +11,8 @@
     appId: "1:8417668413:web:22b7b111e797949616a954"
   };
 
-  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-</script>
+  export const db = getFirestore(app);
+
 
 
