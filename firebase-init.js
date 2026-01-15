7532068ca3a4c73548f821f0caceea2a099350c4
poldani2024 <pedro.l.oldani@gmail.com>
2026-01-15 12:35:24 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,18 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyATJicgCiFF3d_EUUw5GQbzPox5M2uSkcw",
+  authDomain: "logistica-d964e.firebaseapp.com",
+  projectId: "logistica-d964e",
+  storageBucket: "logistica-d964e.firebasestorage.app",
+  messagingSenderId: "8417668413",
+  appId: "1:8417668413:web:22b7b111e797949616a954"
+};
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
- 
-  const firebaseConfig = {
-    apiKey: "AIzaSyATJicgCiFF3d_EUUw5GQbzPox5M2uSkcw",
-    authDomain: "logistica-d964e.firebaseapp.com",
-    projectId: "logistica-d964e",
-    storageBucket: "logistica-d964e.firebasestorage.app",
-    messagingSenderId: "8417668413",
-    appId: "1:8417668413:web:22b7b111e797949616a954"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  export const db = getFirestore(app);
-
-
-
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
