1d30c7ccbd6a6b35eb5c77f55a077a2a2ba52633
poldani2024 <pedro.l.oldani@gmail.com>
2026-02-12 00:17:18 -0300

    Update core.js

--- core.js
+++ core.js
@@ -51,13 +51,17 @@ export const STATE = {
     assignments: new Map()      // driverId -> { passengerIds: [] }
   }
 };
-window.waitForAuth = function(){
+window.waitForAuth = function () {
   return new Promise((resolve) => {
     const unsub = firebase.auth().onAuthStateChanged((user) => {
+      window.STATE = window.STATE || {};
       STATE.auth = STATE.auth || {};
       STATE.auth.user = user || null;
       STATE.auth.isAdmin = !!(user && user.email === "pedro.l.oldani@gmail.com");
-      if (user){ unsub(); resolve(user); }
+      if (user) {
+        unsub();
+        resolve(user);
+      }
     });
   });
 };
